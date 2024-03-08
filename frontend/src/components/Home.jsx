6ed84f9aef162671f587b5c8b7a746cb6041e7a8
Dwij13 <dwijjindal2005@gmail.com>
2024-03-09 03:45:01 +0530

    Donation page css

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,8 +1,9 @@
 import React from "react";
-
+import { Center, SimpleGrid } from "@chakra-ui/react";
 import back from "./../assets/back.png";
 import { Button } from "@chakra-ui/button";
 import { useNavigate } from "react-router";
+import bgImg from "./../assets/bg-img.png";
 
 const Home = () => {
   const navigate = useNavigate();
@@ -14,27 +15,29 @@ const Home = () => {
   }
 
   return (
-    <div className="home-parent">
-      <div className="moto-div">
-        <div className="moto">Turning Trash into Treasure</div>
-        <div className="sub-moto">
-          Revolutionizing Waste Management Together
+    <>
+      <div className="home-parent">
+        <div className="moto-div">
+          <div className="moto">Turning Trash into Treasure</div>
+          <div className="sub-moto">
+            Revolutionizing Waste Management Together
+          </div>
+          <a onClick={regiNavigate}>Get Started with society registeration--</a>
+          <Button
+            width="11vmax"
+            backgroundColor="#fefed4"
+            color="green"
+            border="3px green solid"
+            onClick={eduNavigation}
+          >
+            Learn More
+          </Button>
         </div>
-        <a onClick={regiNavigate}>Get Started with society registeration--</a>
-        <Button
-          width="11vmax"
-          backgroundColor="#fefed4"
-          color="green"
-          border="3px green solid"
-          onClick={eduNavigation}
-        >
-          Learn More
-        </Button>
+        <div className="right-img-div">
+          <img className="right-img" src={bgImg} />
+        </div>
       </div>
-      <div className="right-img-div">
-        <img className="right-img" src={back} />
-      </div>
-    </div>
+    </>
   );
 };
 
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -24,23 +24,25 @@ export default function Navbar() {
   const [isMobile, setIsMobile] = useState(false);
   const { login, setLogin } = useContext(AppContext);
   const navigate = useNavigate();
-  const logout = ()=>{
-    deleteCookie("username")
-    deleteCookie("auth-token")
-    setLogin(loginCheck())
-    window.reload()
-  }
+  const logout = () => {
+    deleteCookie("username");
+    deleteCookie("auth-token");
+    setLogin(loginCheck());
+    window.reload();
+  };
   const loginBtn = () => {
     if (login) {
       return (
-          <MenuItem onClick={logout} icon={<TbLogin2 />}>Logout</MenuItem>
+        <MenuItem onClick={logout} icon={<TbLogin2 />}>
+          Logout
+        </MenuItem>
       );
-    }else{
-      return(
+    } else {
+      return (
         <Link to="/signup">
           <MenuItem icon={<TbLogin2 />}>Login / Signup</MenuItem>
         </Link>
-      )
+      );
     }
   };
 
@@ -48,12 +50,13 @@ export default function Navbar() {
     if (login) {
       return (
         <MenuButton
-        onClick={logout}
+          onClick={logout}
           as={Button}
           leftIcon={<TbLogin2 />}
           sx={{
-            backgroundColor: "green",
+            backgroundColor: "#00a200",
             color: "white",
+            fontSize: "1.2vmax",
             ":hover": {
               backgroundColor: "green",
             },
@@ -65,30 +68,30 @@ export default function Navbar() {
           Logout
         </MenuButton>
       );
-    }else{
-      return(
+    } else {
+      return (
         <Link to="/signup">
-                    <MenuButton
-                      as={Button}
-                      leftIcon={<TbLogin2 />}
-                      sx={{
-                        backgroundColor: "green",
-                        color: "white",
-                        ":hover": {
-                          backgroundColor: "green",
-                        },
-                        ":active": {
-                          backgroundColor: "green",
-                        },
-                      }}
-                    >
-                      Login / Signup
-                    </MenuButton>
-                  </Link>
-      )
+          <MenuButton
+            as={Button}
+            leftIcon={<TbLogin2 />}
+            sx={{
+              backgroundColor: "#00a200",
+              color: "white",
+              fontSize: "1.2vmax",
+              ":hover": {
+                backgroundColor: "green",
+              },
+              ":active": {
+                backgroundColor: "green",
+              },
+            }}
+          >
+            Login / Signup
+          </MenuButton>
+        </Link>
+      );
     }
   };
-
 
   useEffect(() => {
     function handleResize() {
@@ -107,7 +110,7 @@ export default function Navbar() {
         <div className="nav-parent-2 flex">
           <div className="nav-logo">
             <Link to="/">
-            <div className="title-nav">WasteX</div>
+              <div className="title-nav">WasteX</div>
             </Link>
           </div>
           <div className="nav-options flex">
@@ -120,7 +123,9 @@ export default function Navbar() {
                   variant="outline"
                   _hover={{ bg: "darkgreen" }}
                   _active={{ bg: "green" }}
-                  sx={{ borderColor: "transparent" }} // Set border color to transparent
+                  sx={{
+                    borderColor: "transparent",
+                  }} // Set border color to transparent
                 />
                 <MenuList>
                   <Link to="/complaint">
@@ -140,9 +145,7 @@ export default function Navbar() {
                   </Link>
 
                   <Link to="/faq">
-                    <MenuItem icon={<FaClipboardQuestion />}>
-                      FAQ
-                    </MenuItem>
+                    <MenuItem icon={<FaClipboardQuestion />}>FAQ</MenuItem>
                   </Link>
 
                   <Link to="/about">
@@ -158,8 +161,9 @@ export default function Navbar() {
                     as={Button}
                     rightIcon={<ChevronDownIcon />}
                     sx={{
-                      backgroundColor: "green",
+                      backgroundColor: "#00a200",
                       color: "white",
+                      fontSize: "1.2vmax",
                       ":hover": {
                         backgroundColor: "darkgreen",
                       },
@@ -170,7 +174,7 @@ export default function Navbar() {
                   >
                     Services
                   </MenuButton>
-                  
+
                   <MenuList>
                     <Link to="/complaint">
                       <MenuItem minH="40px">
@@ -202,7 +206,7 @@ export default function Navbar() {
                         &nbsp;
                         <span>Existing Complaints</span>
                       </MenuItem>
-                      </Link>
+                    </Link>
                   </MenuList>
                 </Menu>
 
@@ -212,8 +216,9 @@ export default function Navbar() {
                       as={Button}
                       leftIcon={<RiTeamFill />}
                       sx={{
-                        backgroundColor: "green",
+                        backgroundColor: "#00a200",
                         color: "white",
+                        fontSize: "1.2vmax",
                         ":hover": {
                           backgroundColor: "darkgreen",
                         },
@@ -233,8 +238,9 @@ export default function Navbar() {
                       as={Button}
                       leftIcon={<FaClipboardQuestion />}
                       sx={{
-                        backgroundColor: "green",
+                        backgroundColor: "#00a200",
                         color: "white",
+                        fontSize: "1.2vmax",
                         ":hover": {
                           backgroundColor: "darkgreen",
                         },
@@ -248,9 +254,7 @@ export default function Navbar() {
                   </Link>
                 </Menu>
 
-                <Menu>
-                  {loginBtn2()}
-                </Menu>
+                <Menu>{loginBtn2()}</Menu>
               </>
             )}
           </div>
