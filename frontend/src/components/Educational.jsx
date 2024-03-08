9749f1402934675b46a762ec5897ff1930efb565
aayushharora <aayushh2k@gmail.com>
2024-03-09 03:59:58 +0530

    Education

--- frontend/src/components/Educational.jsx
+++ frontend/src/components/Educational.jsx
@@ -3,6 +3,8 @@ import { FaArrowCircleRight, FaArrowCircleLeft } from "react-icons/fa";
 import { Button } from "@chakra-ui/react";
 import data from "./data.json";
 import { useNavigate } from "react-router-dom";
+import Book from "../assets/BookClipart.png"
+import Ques from "../assets/Question.png"
 
 export default function Educational() {
   const [count, setCount] = useState(0);
@@ -33,33 +35,67 @@ export default function Educational() {
 
   return (
     <div className="edu-parent">
+
+
+{/* <div className="bg-elements" style={{top:"20vh", left:"10vw", bottom:"30vh"}}>
+        <img src={Book} alt="" id="back-img-1"/>
+      </div>
+
+      <div className="bg-elements" style={{top:"80vh", right:"30vw", left:"20vw", bottom:"30vh"}}>
+        <img src={Ques} alt="" id="back-img-2"/>
+      </div>
+
+
+      <div className="bg-elements" style={{top:"15vh", right:"30vw", left:"20vw", bottom:"30vh"}}>
+        <img src={Book} alt="" id="back-img-1"/>
+      </div>
+
+      <div className="bg-elements" style={{top:"15vh", right:"30vw", left:"20vw", bottom:"30vh"}}>
+        <img src={Ques} alt="" id="back-img-2"/>
+      </div>
+      <div className="bg-elements" style={{top:"15vh", right:"30vw", left:"20vw", bottom:"30vh"}}>
+        <img src={Book} alt="" id="back-img-1"/>
+      </div>
+      <div className="bg-elements" style={{top:"15vh", right:"30vw", left:"20vw", bottom:"30vh"}}>
+        <img src={Ques} alt="" id="back-img-2"/>
+      </div>
+      <div className="bg-elements" style={{top:"15vh", right:"30vw", left:"20vw", bottom:"30vh"}}>
+        <img src={Book} alt="" id="back-img-1"/>
+      </div>
+ */}
+
+
+
+
       <div className="edu-div flex">
         <div className="content">
-          <div className="edu-text flex">{data[count].text}</div>
           <div className="edu-video">
             <img src={data[count].gif} id="edu-img" />
           </div>
+          <div className="edu-text">
+            <div className="edu-text-1">
+              Title
+            </div>
+            <div className="edu-text-2">
+            {data[count].text}
+            </div>
+            </div>
         </div>
         <div className="nav-buttons">
           <FaArrowCircleLeft
-            size="2.5vmax"
+            size="2.8vmax"
             onClick={prevData}
             style={{ color: clicked ? "darkgreen" : "green" }}
             id="arrow"
           />
           <FaArrowCircleRight
-            size="2.5vmax"
+            size="2.8vmax"
             onClick={nextData}
             style={{ color: clicked ? "darkgreen" : "green" }}
             id="arrow"
           />
         </div>
-        <Button
-          colorScheme="green"
-          onClick={handleBack}
-        >
-          Back
-        </Button>
+
       </div>
     </div>
   );
